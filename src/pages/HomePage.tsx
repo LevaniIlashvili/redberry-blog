10a13c1a655ded3cb1718ea0b099c3ee29ceac73
LevaniIlashvili <levaniko900@gmail.com>
2023-12-19 16:45:41 +0400

    add hero

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,43 @@
+import styled from "styled-components";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Wrapper>
+      <h1>ბლოგი</h1>
+      <img
+        className="galaxy-logo"
+        src="src/assets/galaxy-logo.svg"
+        alt="galaxy logo"
+        width={1024}
+        height={355}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  background-color: var(--page-background);
+  min-height: calc(100vh - 8rem);
+  position: relative;
+
+  h1 {
+    width: 22.5rem;
+    color: var(--black);
+    font-size: 6.4rem;
+    font-weight: 700;
+    line-height: 7.2rem;
+    position: absolute;
+    left: 8.9rem;
+    top: 20.9rem;
+  }
+
+  .galaxy-logo {
+    width: 62.4rem;
+    height: 20rem;
+    position: absolute;
+    right: 7.6rem;
+    top: 14.4rem;
+  }
+`;
 
 export default HomePage;
