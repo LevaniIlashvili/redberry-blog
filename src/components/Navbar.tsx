0906011a1d849b63789dba6162ae9dc7e241dac0
LevaniIlashvili <levaniko900@gmail.com>
2023-12-21 12:06:57 +0400

    enhance navbar functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,23 +2,39 @@ import { useState } from "react";
 import styled from "styled-components";
 import LoginModal from "./LoginModal";
 import { useAppSelector } from "../app/hooks";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const isLoggedIn = useAppSelector((state) => state.user.loggedIn);
 
   return (
-    <Wrapper>
+    <Wrapper
+      style={{
+        justifyContent: `${
+          currentPath === "/add-blog" ? "center" : "space-between"
+        }`,
+      }}
+    >
       <img
         src="src/assets/redberry-logo.svg"
         alt="logo"
+        className="redberry-logo"
         width={150}
         height={24}
+        onClick={() => navigate("/")}
       />
       {!isLoggedIn ? (
         <button onClick={() => setIsModalOpen((prev) => !prev)}>შესვლა</button>
       ) : (
-        <button>დაამატე ბლოგი</button>
+        <>
+          {currentPath !== "/add-blog" && (
+            <button onClick={() => navigate("/add-blog")}>დაამატე ბლოგი</button>
+          )}
+        </>
       )}
       {isModalOpen && <LoginModal setIsModalOpen={setIsModalOpen} />}
     </Wrapper>
@@ -30,9 +46,12 @@ const Wrapper = styled.nav`
   background-color: var(--white);
   display: flex;
   align-items: center;
-  justify-content: space-between;
   padding: 0 7.6rem;
   border-bottom: 1px solid #e4e3eb;
+
+  .redberry-logo {
+    cursor: pointer;
+  }
 
   button {
     background-color: var(--button-color-blue);
