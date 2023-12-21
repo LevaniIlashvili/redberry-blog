b1c462cf2e4f926c813df6df03918cdf66c87b08
LevaniIlashvili <levaniko900@gmail.com>
2023-12-21 12:10:54 +0400

    empty page

---
+++ src/pages/AddBlogPage.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const AddBlogPage = () => {
+  return <Wrapper>add blog</Wrapper>;
+};
+
+const Wrapper = styled.main`
+  background-color: var(--page-background);
+  min-height: calc(100vh - 8rem);
+`;
+
+export default AddBlogPage;
