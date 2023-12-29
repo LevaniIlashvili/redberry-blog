1cd22bc37a7a93c793e8aa8dcd56c82ca7b8c7f1
LevaniIlashvili <levaniko900@gmail.com>
2023-12-29 12:40:22 +0400

    move fetching blogs to app component

--- src/components/BlogFeed.tsx
+++ src/components/BlogFeed.tsx
@@ -1,35 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 import BlogCard from "./BlogCard";
-import { BlogType } from "../types/types";
 import { useAppSelector } from "../app/hooks";
 
 const BlogFeed = () => {
-  const [blogs, setBlogs] = useState<BlogType[]>([]);
+  const blogs = useAppSelector((state) => state.blogs.blogs);
   const selectedCategories = useAppSelector(
     (state) => state.categories.selectedCategories
   );
-
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        const res = await axios.get(
-          "https://api.blog.redberryinternship.ge/api/blogs",
-          {
-            headers: {
-              Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
-            },
-          }
-        );
-        console.log(res);
-        setBlogs(res.data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchBlogs();
-  }, []);
 
   return (
     <Wrapper>
@@ -51,7 +28,7 @@ const Wrapper = styled.section`
   width: 100%;
   margin-top: 6.4rem;
   display: grid;
-  grid-template-columns: repeat(auto-fill, 40.8rem);
+  grid-template-columns: repeat(3, 40.8rem);
   grid-gap: 3.2rem;
   justify-content: center;
 `;
