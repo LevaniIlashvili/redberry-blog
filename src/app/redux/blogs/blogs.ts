17ea36d1fb6f2f697cff4ccebb8c6be01031d532
LevaniIlashvili <levaniko900@gmail.com>
2023-12-29 15:31:36 +0400

    filter blogs based on publish date

--- src/app/redux/blogs/blogs.ts
+++ src/app/redux/blogs/blogs.ts
@@ -14,7 +14,27 @@ const blogsSlice = createSlice({
   initialState,
   reducers: {
     setBlogs: (state, action) => {
-      state.blogs = action.payload;
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth() + 1; // Months are zero-indexed
+      const currentDay = currentDate.getDate();
+
+      const blogs = [...action.payload].filter((blog) => {
+        const publishDate = new Date(blog.publish_date);
+        const publishYear = publishDate.getFullYear();
+        const publishMonth = publishDate.getMonth() + 1;
+        const publishDay = publishDate.getDate();
+
+        return (
+          publishYear < currentYear ||
+          (publishYear === currentYear && publishMonth < currentMonth) ||
+          (publishYear === currentYear &&
+            publishMonth === currentMonth &&
+            publishDay <= currentDay)
+        );
+      });
+
+      state.blogs = blogs;
     },
   },
 });
